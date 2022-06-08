6489bda860b0d8c68eba1f55de24e6bc490cded1
reload1bronze <nature9th@gmail.com>
2022-06-08 23:59:07 +0900

    feat: initial commit

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Carrot Marget Clone',
+      theme: ThemeData(
+        primarySwatch: Colors.deepOrange,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('바니바니'),
+      ),
+      body: Container(
+        color: Colors.amber,
+      ),
+    );
+  }
+}
