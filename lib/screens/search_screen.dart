fc9ab2c5c1b2a6e75ee6c5b65dd23af6917340a3
reload1bronze <nature9th@gmail.com>
2022-06-17 17:16:45 +0900

    feat: get location (lat, long)

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:location/location.dart';
 
 import '../data/address_model.dart';
 import '../utils/logger.dart';
@@ -47,7 +48,32 @@ class _SearchScreenState extends State<SearchScreen> {
             },
           ),
           TextButton.icon(
-            onPressed: () {},
+            onPressed: () async {
+              Location location = new Location();
+
+              bool _serviceEnabled;
+              PermissionStatus _permissionGranted;
+              LocationData _locationData;
+
+              _serviceEnabled = await location.serviceEnabled();
+              if (!_serviceEnabled) {
+                _serviceEnabled = await location.requestService();
+                if (!_serviceEnabled) {
+                  return;
+                }
+              }
+
+              _permissionGranted = await location.hasPermission();
+              if (_permissionGranted == PermissionStatus.denied) {
+                _permissionGranted = await location.requestPermission();
+                if (_permissionGranted != PermissionStatus.granted) {
+                  return;
+                }
+              }
+
+              _locationData = await location.getLocation();
+              logger.d(_locationData);
+            },
             icon: Icon(
               Icons.location_pin,
               color: Colors.white,
